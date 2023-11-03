1c16e9bfa8feee177b1b9b67a747cfd664ac9095
iadokonat <iadokonat0106@gmail.com>
2023-11-03 15:03:58 +0900

    update: Make the sprite draggable.

--- src/main.ts
+++ src/main.ts
@@ -1,56 +1,72 @@
-import { Application, Sprite, Assets } from 'pixi.js'
+import { Application, Sprite, Assets, FederatedPointerEvent } from 'pixi.js'
 import cupImage from '@/assets/images/cup.png'
-import doveImage from '@/assets/images/dove.png'
 import './style.css'
+import { addSprite } from './utils/add-sprite'
 
-// The application will create a renderer using WebGL, if possible,
-// with a fallback to a canvas render. It will also setup the ticker
-// and the root stage PIXI.Container
+type AddCupReturnType = Promise<{
+  cup: Sprite
+  destroy: () => void
+}>
+
+let dragTarget: Sprite | null = null
+
+const addCup = async (app: Application): AddCupReturnType => {
+  const texture = await Assets.load(cupImage)
+  const { sprite, destroy } = addSprite(app, texture)
+
+  return { cup: sprite, destroy }
+}
+
 const app = new Application({
   view: document.querySelector('#canvas') as HTMLCanvasElement,
-  width: 400,
-  height: 300,
+  width: 800,
+  height: 600,
   // autoDensity: true,
   // resizeTo: window,
   // powerPreference: 'high-performance',
   backgroundColor: 0x1099bb,
 })
 
-const texture = await Assets.load(cupImage)
-
-const cup = new Sprite(texture)
-const cupAspectRatio = cup.width / cup.height
-
-cup.width = 100
-cup.height = cup.width / cupAspectRatio
-
-cup.x = app.renderer.width / 2
-cup.y = app.renderer.height / 2
-
-cup.anchor.x = 0.5
-cup.anchor.y = 0.5
-
-const doveTexture = await Assets.load(doveImage)
-
-const dove = new Sprite(doveTexture)
-const doveAspectRatio = dove.width / dove.height
-
-dove.width = 100
-dove.height = dove.width / doveAspectRatio
-
-dove.x = app.renderer.width / 4
-dove.y = app.renderer.height / 4
-
-dove.anchor.x = 0.5
-dove.anchor.y = 0.5
-
-app.stage.addChild(cup, dove)
-
-const animate = (deltaTime: number) => {
-  cup.rotation += deltaTime / 100
-  dove.rotation -= deltaTime / 100
+const onDragMove = (event: FederatedPointerEvent) => {
+  if (dragTarget) {
+    dragTarget.parent.toLocal(event.global, undefined, dragTarget.position)
+  }
 }
 
-app.ticker.add(animate)
+const onDragStart = (sprite: Sprite) => {
+  sprite.alpha = 0.5
+  dragTarget = sprite
+  app.stage.on('pointermove', onDragMove)
+}
 
-// app.ticker.remove(animate)
+const onDragEnd = () => {
+  if (dragTarget) {
+    app.stage.off('pointermove', onDragMove)
+    dragTarget.alpha = 1
+    dragTarget = null
+  }
+}
+
+app.stage.eventMode = 'static'
+app.stage.hitArea = app.screen
+app.stage.on('pointerup', onDragEnd)
+app.stage.on('pointerupoutside', onDragEnd)
+
+Promise.all(
+  Array(5)
+    .fill(null)
+    .map((_) => addCup(app))
+).then((cups) => {
+  cups.forEach(({ cup }, i) => {
+    cup.x =
+      (((app.renderer.width * i) / (cups.length - 1)) * (app.renderer.width - cup.width)) / app.renderer.width +
+      cup.width / 2
+    cup.y =
+      (((app.renderer.height * i) / (cups.length - 1)) * (app.renderer.height - cup.height)) / app.renderer.height +
+      cup.height / 2
+    cup.on('pointerdown', () => onDragStart(cup))
+  })
+})
+
+// app.stage.removeChild(cup)
+// cup.destroy()
---
+++ src/utils/add-sprite.ts
@@ -0,0 +1,27 @@
+import { Application, Sprite, Texture } from 'pixi.js'
+
+export const addSprite = (app: Application, texture: Texture): { sprite: Sprite; destroy: () => void } => {
+  const sprite = new Sprite(texture)
+  const spriteAspectRatio = sprite.width / sprite.height
+
+  const destroy = (): void => {
+    app.stage.removeChild(sprite)
+    sprite.destroy()
+  }
+
+  sprite.eventMode = 'static'
+  sprite.cursor = 'pointer'
+
+  sprite.width = 100
+  sprite.height = sprite.width / spriteAspectRatio
+
+  sprite.x = app.renderer.width / 2
+  sprite.y = app.renderer.height / 2
+
+  sprite.anchor.x = 0.5
+  sprite.anchor.y = 0.5
+
+  app.stage.addChild(sprite)
+
+  return { sprite, destroy }
+}
